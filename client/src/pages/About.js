bbb3ae10516bffe4f771dfb71503dbf5d18ed03b
Vipin Kargeti <81311511+Vipin-Kargeti@users.noreply.github.com>
2023-07-02 01:09:16 +0530

    commit

---
+++ client/src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout title={"About us - Ecommer app"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+            Introducing our cutting-edge e-commerce app, designed to
+            revolutionize your shopping experience. With our app, you can
+            explore a vast collection of products and enjoy the convenience of
+            online shopping right at your fingertips.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
